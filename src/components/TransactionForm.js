6471dc793abe22c39f774ad95bcd32dbee10d86d
anthonymjimenez <anthonyjim96@gmail.com>
2020-12-02 23:24:03 -0500

    appended crypto info in blockfolio making it easier todisplay the data we need in the comp

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {showurl, isoId, owned, financial } from '../utils/utils'
+import { financial } from '../utils/utils'
 
 export default function TransactionForm({ crypto, user, updateBalance }) {
   const [state, setState] = useState({
